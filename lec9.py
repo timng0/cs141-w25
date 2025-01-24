069837a1a3ec6a8cd3cd5428782956ab41e6e939
Tim Ng <timng@uchicago.edu>
2025-01-24 17:25:38 -0600

    lec9 init

---
+++ lec9.py
@@ -0,0 +1,27 @@
+"""
+Lecture 9
+
+Last class:
+    Mapping
+    Mutation
+    Iteration
+
+Today:
+    Tuples
+    Strings
+"""
+
+counts = [2, 4, 3, 4, 3, 1, 9, 3]
+
+# the usual iteration
+for num in counts:
+    print(num)
+
+# iterating on index: bad
+for i in range(len(counts)):
+    print(i, counts[i])
+
+# iterating via enumeration: good
+for i, num in enumerate(counts):
+    print(i, counts[i])
+
