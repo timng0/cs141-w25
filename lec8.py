df934a014182a105c7c2b0b66ec15619d5ee94c6
Tim Ng <timng@uchicago.edu>
2025-01-24 09:28:10 -0600

    lec8 init

---
+++ lec8.py
@@ -0,0 +1,29 @@
+"""
+Lecture 8
+
+Last class:
+    Primality testing is secretly accumulation
+    Short-circuiting with break
+    Nested loops
+    More on lists
+
+Today:
+    Mapping
+    Mutation
+    Iteration
+    Tuples?
+"""
+
+strings = ["cat", "", "pepper", "shoe", "paper"]
+lengths = []
+for string in strings:
+    lengths = lengths + [len(string)]
+
+### The "mapping" pattern
+<new_list> = []     # initialize empty list
+for <item> in <list>:
+    # do something with the item to create a new item
+    <new_item> = ... <item> ...
+    # add new item to list
+    <new_list> = <new_list> + [<new_item>]
+
