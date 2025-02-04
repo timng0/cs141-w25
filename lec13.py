5576eab67a1308d09c1af100c9c86c164d87391f
Tim Ng <timng@uchicago.edu>
2025-02-04 15:55:17 -0600

    start lec13

---
+++ lec13.py
@@ -0,0 +1,29 @@
+"""
+Lecture 13
+
+Last class:
+    Tying up parameters and scope
+    Dictionaries
+
+Today:
+    Midterm information
+    More on dictionaries
+"""
+
+def count_all_words(words):
+    """
+    Count the number of occurrences of each word in the list
+
+    Input:
+        words (list[str]): list of words
+    
+    Output (dict[str,int]): dictionary that maps a string to
+        the number of times it appears in the list
+    """
+    counts = {}
+    for word in words:
+        if word not in counts:
+            counts[word] = 1
+        else:
+            counts[word] = counts[word] + 1
+    return counts
