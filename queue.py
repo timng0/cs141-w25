463e129d11b5933f6e681bcbb4a939b0edc13612
Tim Ng <timng@uchicago.edu>
2025-02-07 09:20:59 -0600

    data structures

---
+++ queue.py
@@ -0,0 +1,51 @@
+def create():
+    """
+    Creates an empty queue.
+
+    Input:
+        None
+
+    Output: empty queue (Queue)
+    """
+    return []
+
+def is_empty(qu):
+    """
+    Determines whether the given queue is empty.
+
+    Input:
+        qu (Queue): the queue
+
+    Output: True if queue is empty, False otherwise (bool)
+    """
+    return qu == []
+
+def enqueue(qu, item):
+    """
+    Adds an item to the back of the queue.
+
+    Input:
+        qu (Queue): the queue
+        item (Any): a value
+
+    Output: None
+    """
+    qu.append(item)
+
+def dequeue(qu):
+    """
+    Removes the item at the front of the queue and produces it.
+
+    Input:
+        qu (Queue): the queue
+
+    Output: item at the front of the queue (Any)
+    """
+    return qu.pop(0)
+
+def to_string(qu):
+    s = "[Front]\n"
+    for item in qu:
+        s += str(item) + "\n"
+    s += "[Back]\n"
+    return s
---
+++ stack.py
@@ -0,0 +1,39 @@
+def create():
+    """
+    Creates an empty stack.
+
+    Input:
+        None
+
+    Output: empty stack (Stack)
+    """
+
+def is_empty(st):
+    """
+    Determine whether the given stack is empty
+    Input:
+        st (Stack): the stack
+
+    Output: True if stack is empty, False otherwise (bool)
+    """
+
+def push(st, item):
+    """
+    Puts an item on the top of the stack.
+
+    Input:
+        st (Stack): the stack
+        item (Any): an item
+
+    Output: None
+    """
+
+def pop(st):
+    """
+    Removes the item on the top of the stack and produces it.
+
+    Input:
+        st (Stack): the stack
+
+    Output: the item on the top of the stack (Any)
+    """
