2a896c79e5d17d35a133222e00521cc5f236c20f
Tim Ng <timng@uchicago.edu>
2025-02-07 09:18:48 -0600

    lec14 updates

---
+++ lec14.py
@@ -0,0 +1,24 @@
+"""
+Lecture 14
+
+Last class:
+    More on dictionaries
+        Complexity
+        Sets
+
+Today:
+    Representing things 
+    Abstract data types
+"""
+colour = (202, 21, 31)
+
+datetime = (2025, 2, 7, 13, 30, 57)
+
+hw1 = {"name": "Homework #1",
+       "short_name": "hw1",
+       "deadline": "2025/01/07",
+       "num_submissions": 191}
+
+table = [['name', 'short_name', 'deadline', 'num_submissions'],
+         ['Homework #1', 'hw1', '2025/01/07', 191]]
+
