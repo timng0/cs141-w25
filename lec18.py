b4a41554ab0171083b465be51e6edd93e3f17074
Tim Ng <timng@uchicago.edu>
2025-02-16 23:23:26 -0600

    lec18 init

---
+++ lec18.py
@@ -0,0 +1,21 @@
+"""
+Lecture 18
+
+Last class:
+    More modeling with classes (Divvy)
+
+Today:
+    Recursion
+    What are natural numbers?
+    Everything in computer science is secretly recursion
+"""
+
+def factorial(n):
+    """
+    Computes the factorial of a given natural number n.
+
+    Input: 
+        n (int): a natural number (i.e. n >= 0)
+
+    Output (int): n!
+    """
