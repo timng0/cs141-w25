7672d57b632cb57a8d80d6b976dc779dcb346efb
Tim Ng <timng@uchicago.edu>
2025-02-19 11:23:55 -0600

    lec19

--- lec19.py
+++ lec19.py
@@ -1,5 +1,5 @@
 """
-Lecture 18
+Lecture 19
 
 Last class:
     Recursion
@@ -11,18 +11,60 @@ Today:
     Sorting
 """
 
-def strings_lengths(strings):
+def strings_lengths(strings, sum_so_far):
     """
     Given a list of strings, produces the sum of lengths of strings in 
     the list.
     
     Input:
         strings (list[str]): List of strings
+        sum_so_far (int): sum of lengths of strings seen so far
 
     Output (int): sum of lengths of strings from strings
     """
     if strings == []:
-        return 0
+        return sum_so_far
     else:
         first, *rest = strings
-        return len(first) + strings_lengths(rest)
+        print(f'strings_lengths({rest}, len({first}) + {sum_so_far}))')
+        return strings_lengths(rest, len(first) + sum_so_far) 
+    
+    total = 0
+    for string in strings:
+        total = total + len(string)
+    return total
+
+def insert(item, lst):
+    """
+    Insert item into the right place in a sorted list.
+
+    Input:
+        item (int): the item
+        lst (list[int]): sorted list of integers
+
+    Output (list[int]): a list with item in the right spot
+    """
+    if lst == []:
+        return [item]
+    else:
+        first, *rest = lst
+        if item < first:
+            return [item] + lst
+        else:
+            return [first] + insert(item, rest)
+
+
+def sort(lst):
+    """
+    Produce a list with the same numbers but in sorted order.
+
+    Input:
+        lst (list[int]): list of integers
+
+    Output (list[int]): the same list but sorted in increasing order
+    """
+    if lst == []:
+        return []
+    else:
+        first, *rest = lst
+        return insert(first, sort(rest))
