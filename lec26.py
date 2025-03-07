6ff1aaffc67cb3e8a5bf55761a7862b14b92c941
Tim Ng <timng@uchicago.edu>
2025-03-07 11:26:41 -0600

    lec26 init

--- lec26.py
+++ lec26.py
@@ -8,3 +8,24 @@ Last class:
 Today:
     Q&A
 """
+
+def fun(lst):
+    if lst == []:
+        asdf
+    else:
+        first, *rest = lst
+        fjeiowajfeowai
+
+def fun(lst):
+    match lst:
+        case []:
+            fjeiofeja
+        case first, *rest:
+            first restjeiajeo
+
+match tree:
+    case Terminal(a):
+        a 
+    case Variable(v, subtrees):
+        v
+        subtreses
